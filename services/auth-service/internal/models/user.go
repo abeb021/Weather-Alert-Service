05ed7f99756cf3847514259a6afc87e23d41fa03
amir <am.gabdullin@mail.ru>
2026-05-09 21:43:43 +0300

    add db method for creation user and token #9

--- services/auth-service/internal/models/user.go
+++ services/auth-service/internal/models/user.go
@@ -6,14 +6,6 @@ import (
 	"github.com/google/uuid"
 )
 
-type RefreshToken struct {
-	ID        uuid.UUID `db:"id"`
-	Token     string    `db:"token"`
-	ExpiresAt time.Time `db:"expires_at"`
-	IsRevoked bool      `db:"is_revoked"`
-	UserID    uuid.UUID `db:"user_id"`
-	CreatedAt time.Time `db:"created_at"`
-}
 type User struct {
 	ID           uuid.UUID `db:"id"`
 	Email        string    `db:"email"`
