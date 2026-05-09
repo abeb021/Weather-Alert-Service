a6ead86aab2ac3b78d19c7929b1d353914ac264f
amir <am.gabdullin@mail.ru>
2026-05-09 21:31:01 +0300

    add models #9

--- services/auth-service/internal/models/user.go
+++ services/auth-service/internal/models/user.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type RefreshToken struct {
+	ID        uuid.UUID `db:"id"`
+	Token     string    `db:"token"`
+	ExpiresAt time.Time `db:"expires_at"`
+	IsRevoked bool      `db:"is_revoked"`
+	UserID    uuid.UUID `db:"user_id"`
+	CreatedAt time.Time `db:"created_at"`
+}
+type User struct {
+	ID           uuid.UUID `db:"id"`
+	Email        string    `db:"email"`
+	PasswordHash string    `db:"password_hash"`
+	CreatedAt    time.Time `db:"created_at"`
+}
