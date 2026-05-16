8d9eaf9ec22d29ae2c1cdd0333f605a73b8c639e
amir <am.gabdullin@mail.ru>
2026-05-16 13:04:26 +0300

    add log printing and auth method in session.go

--- services/notification-service/internal/transport/smtp/session.go
+++ services/notification-service/internal/transport/smtp/session.go
@@ -1,9 +1,16 @@
 package smtp
 
 import (
+	"errors"
 	"io"
+	"log/slog"
 
 	gosmtp "github.com/emersion/go-smtp"
+)
+
+var (
+	ErrReadData = errors.New("failed to read data of message")
+	ErrAuth     = errors.New("invalid username or password")
 )
 
 type NotificationService interface {
@@ -11,6 +18,7 @@ type NotificationService interface {
 }
 
 type Session struct {
+	logger              *slog.Logger
 	notificationService NotificationService
 
 	from string
@@ -24,11 +32,15 @@ func NewSession(notificationService NotificationService) *Session {
 }
 
 func (s *Session) Mail(from string, _ *gosmtp.MailOptions) error {
+	s.logger.Info("Mail from:", from)
+
 	s.from = from
 	return nil
 }
 
 func (s *Session) Rcpt(to string, _ *gosmtp.RcptOptions) error {
+	s.logger.Info("Rcpt to:", to)
+
 	s.to = append(s.to, to)
 	return nil
 }
@@ -36,8 +48,11 @@ func (s *Session) Rcpt(to string, _ *gosmtp.RcptOptions) error {
 func (s *Session) Data(r io.Reader) error {
 	data, err := io.ReadAll(r)
 	if err != nil {
+		s.logger.Error(ErrReadData.Error(), err)
 		return err
 	}
+
+	s.logger.Info("Received message:", string(data))
 
 	return s.notificationService.SendEmail(s.from, s.to, data)
 }
@@ -45,6 +60,15 @@ func (s *Session) Data(r io.Reader) error {
 func (s *Session) Reset() {
 	s.from = ""
 	s.to = nil
+}
+
+func (s *Session) AuthPlain(username, password string) error {
+	if username != "testuser" || password != "testpass" {
+		s.logger.Error(ErrAuth.Error(), username, password)
+		return ErrAuth
+	}
+
+	return nil
 }
 
 func (s *Session) Logout() error {
